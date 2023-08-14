3fd4e0e29239f7bdfb0d2c83f5723973038d4b39
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-14 23:30:22 +0600

    bank dashboard created

--- js/login.js
+++ js/login.js
@@ -14,9 +14,9 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // Danger: DO NOT VERIFY EMAIL PASSWORD ON THE CLINT SIDE
     // step-4: verify email and password and check whether valid user or not
     if (email === 'sontan@baap.com' && password === 'secret') {
-        console.log('valid user');
+        window.location.href = "bank.html"
     } else {
-        console.log('invalid user');
+        alert('tui password vule gasos!!!! Toke ami teijjo sontan gshona korlalm. ')
     }
 
 })
