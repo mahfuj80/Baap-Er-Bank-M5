bd0e23f3366d73652da3d9257743dc9572d8407b
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-15 05:27:45 +0600

    validation and alert added in withdraw event

--- js/withdraw.js
+++ js/withdraw.js
@@ -18,25 +18,44 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
+    // step-7:
+
+    withdrawField.value = "";
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please Provide a valid Number');
+
+        return;
+    }
     // step-3:
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    // step-4:
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     // step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    // step-7:
+    withdrawField.value = "";
+
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap er bank e eto tk nai.')
+        return;
+    }
+
+    // step-4:
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // step-6:
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step-7:
-    withdrawField.value = "";
+
 
 })
